3306497894540251effaba36fb28cd234dab32dc
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-26 21:05:37 +0600

    folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// bootstrap uses
+import Button from 'react-bootstrap/Button';
+
+// font awesome uses
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faTwitter,faFacebook,faPinterest, faGithub, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+library.add(faTwitter, faFacebook, faPinterest, faGithub, faWhatsapp, faInstagram, faHouse, faUser)
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+         <Button variant="warning">Warning </Button>
+         <FontAwesomeIcon className='p-5' icon="fa-solid fa-house" />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
