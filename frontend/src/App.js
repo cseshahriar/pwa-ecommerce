e11d1762bbdea2945db723f2545439f70b06f474
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-27 07:45:17 +0600

    add router and assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react'
-
-// bootstrap uses
-import Button from 'react-bootstrap/Button';
-
-// font awesome uses
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-import { faTwitter,faFacebook,faPinterest, faGithub, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
-import { faHouse } from '@fortawesome/free-solid-svg-icons'
-import { faUser } from '@fortawesome/free-regular-svg-icons'
-library.add(faTwitter, faFacebook, faPinterest, faGithub, faWhatsapp, faInstagram, faHouse, faUser)
-
+import React, { Component, Fragment } from 'react'
+import AppRoute from './routes/AppRoute';
 
 export class App extends Component {
   render() {
     return (
-      <div>
-         <Button variant="warning">Warning </Button>
-         <FontAwesomeIcon className='p-5' icon="fa-solid fa-house" />
-      </div>
+     <Fragment>
+        <AppRoute />
+     </Fragment>
     )
   }
 }
---
+++ frontend/src/routes/AppRoute.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react'
+// router 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// components
+import HomePage from '../pages/HomePage';
+
+class AppRoute extends Component {
+  render() {
+    return (
+       <Fragment>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+       </Fragment>
+    )
+  }
+}
+
+export default AppRoute
