58aaa4bf0976ab58e1fec143e7d8fa6339e48f90
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-27 08:05:56 +0600

    app route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // react slick slider
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import custom css
 import './assets/css/custom.css';
-
+import './assets/css/animate.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- frontend/src/routes/AppRoute.js
+++ frontend/src/routes/AppRoute.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from '../pages/HomePage';
 
 class AppRoute extends Component {
+  
   render() {
     return (
        <Fragment>
