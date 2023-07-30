5896933111a58ee12c3ea3014cbc3b68d3b8d40a
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-30 23:10:20 +0600

    mega menu activate

--- frontend/src/components/home/MegaMenu.jsx
+++ frontend/src/components/home/MegaMenu.jsx
@@ -1,36 +1,29 @@
 import React, { Component, Fragment } from "react";
 
 class MegaMenu extends Component {
+
+  constructor() {
+    super();
+  }
+
+  MenuItemClick=(event)=>{
+    event.target.classList.toggle("active");
+    var panel = event.target.nextElementSibling;
+    if(panel.style.maxHeight){
+         panel.style.maxHeight = null;
+    }else{
+         panel.style.maxHeight= panel.scrollHeight+ "px"
+    }
+}
+
+
+
   render() {
     return (
       <div className="accordionMenuDiv">
         <div className="accordionMenuDivInside">
-          <button className="accordion">
-            <img
-              alt=""
-              className="accordionMenuIcon"
-              src="https://www.flaticon.com/free-icon/image-gallery_3342137"
-            />
-            &nbsp; Men's Clothing
-          </button>
-          <div className="panel">
-            <ul>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 2
-                </a>
-              </li>
-            </ul>
-          </div>
 
-          <button className="accordion">
+          <button onClick={this.MenuItemClick} className="accordion">
             <img
               alt=""
               className="accordionMenuIcon"
@@ -55,7 +48,8 @@ class MegaMenu extends Component {
             </ul>
           </div>
 
-          <button className="accordion">
+
+          <button onClick={this.MenuItemClick} className="accordion">
             <img
               alt=""
               className="accordionMenuIcon"
@@ -80,7 +74,59 @@ class MegaMenu extends Component {
             </ul>
           </div>
 
-          <button className="accordion">
+          <button onClick={this.MenuItemClick} className="accordion">
+            <img
+              alt=""
+              className="accordionMenuIcon"
+              src="https://image.flaticon.com/icons/png/128/739/739249.png"
+            />
+            &nbsp; Men's Clothing
+          </button>
+
+          <div className="panel">
+            <ul>
+              <li>
+                <a href="#" className="accordionItem">
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="accordionItem">
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <button onClick={this.MenuItemClick} className="accordion">
+            <img
+              alt=""
+              className="accordionMenuIcon"
+              src="https://image.flaticon.com/icons/png/128/739/739249.png"
+            />
+            &nbsp; Men's Clothing
+          </button>
+
+          <div className="panel">
+            <ul>
+              <li>
+                <a href="#" className="accordionItem">
+                  {" "}
+                  Mans Tshirt 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="accordionItem">
+                  {" "}
+                  Mans Tshirt 2
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <button onClick={this.MenuItemClick} className="accordion">
             <img
               alt=""
               className="accordionMenuIcon"
@@ -105,7 +151,7 @@ class MegaMenu extends Component {
             </ul>
           </div>
 
-          <button className="accordion">
+          <button onClick={this.MenuItemClick} className="accordion">
             <img
               alt=""
               className="accordionMenuIcon"
@@ -130,57 +176,7 @@ class MegaMenu extends Component {
             </ul>
           </div>
 
-          <button className="accordion">
-            <img
-              alt=""
-              className="accordionMenuIcon"
-              src="https://image.flaticon.com/icons/png/128/739/739249.png"
-            />
-            &nbsp; Men's Clothing
-          </button>
-          <div className="panel">
-            <ul>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 2
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <button className="accordion">
-            <img
-              alt=""
-              className="accordionMenuIcon"
-              src="https://image.flaticon.com/icons/png/128/739/739249.png"
-            />
-            &nbsp; Men's Clothing
-          </button>
-          <div className="panel">
-            <ul>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 1
-                </a>
-              </li>
-              <li>
-                <a href="#" className="accordionItem">
-                  {" "}
-                  Mans Tshirt 2
-                </a>
-              </li>
-            </ul>
-          </div>
-
-          <button className="accordion">
+          <button onClick={this.MenuItemClick} className="accordion">
             <img
               alt=""
               className="accordionMenuIcon"
