baf5ce7cb8ece885fbdcb7d0bc823e16e7d5d5b4
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-30 08:58:57 +0600

    feat: collection component design

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,12 +3,14 @@ import React, { Component, Fragment } from 'react'
 // Components imports
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import Categories from '../components/home/Categories'
+import Collection from '../components/home/Collection'
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
           <FeaturedProducts />
+          <Collection />
           <Categories />
       </Fragment>
     )
