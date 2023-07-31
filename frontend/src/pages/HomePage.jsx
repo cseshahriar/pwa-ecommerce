a4ed38b7df7e0d94a3e4c2b111e30062e6393f80
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-07-31 09:12:41 +0600

    feat: desktop menu design

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,16 +6,18 @@ import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import NewArrival from '../components/home/NewArrival'
 import HomeTop from '../components/home/HomeTop'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+          <NavMenuDesktop/>
           <HomeTop />
           <FeaturedProducts />
           <NewArrival />
-          <Collection />
           <Categories />
+          <Collection />
       </Fragment>
     )
   }
